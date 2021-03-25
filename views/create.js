c3baf117b89178413ae22f5ec2467180d1ee55b7
AlinaSadovskaya <alinasadovsckaya@yandex.ru>
2021-03-25 23:41:26 +0300

    change create view

--- views/create.js
+++ views/create.js
@@ -1,21 +1,39 @@
 const create = `
+
 <link rel="stylesheet" href="styles/create.css">
 <link rel="stylesheet" href="styles/ingredients.css">
 <form class="create-coffee-form">
         <div class="coffee-main-div">
 
-          <div class="coffee-left-div">
-            <div class="coffee-image">
-              <img src="images/empty-cup.png" alt="empty-cup">
+          <div class="coffee-round-image">
+            <div class="coffee-left-div">
+                <div class="coffee-image">
+                <img src="images/empty-cup.png" alt="empty-cup">
+                </div>
             </div>
-            <input type="text" id="beauty-input" name="coffee-name" value="" placeholder="ENTER A NAME HERE..." required>
-            <label for="coffee-value">STANDARD VALUE:</label>
-            <input type="number" id="beauty-input-number" name="coffee-value" value="" placeholder="ml" required>
           </div>
 
           <div class="coffee-right-div">
+            <div class="coffee-value-div">
+                <input type="text" id="beauty-input" name="coffee-name" value="" placeholder="ENTER A NAME HERE..." required>
+                <div class="coffee-value-str">
+                  <label for="coffee-value">STANDARD VALUE:</label>
+                  <input type="number" id="beauty-input-number" name="coffee-value" value="" placeholder="ml" required>
+                </div>
+            </div>
             <div class="ingredients-menu">
               <ol class="ingredients-list">
+                    <li class="ingredient-desc">
+                        <div class="ingredient-value">
+                            <select class="ingredients-select" name="ingredients-select">
+                            <option value="espresso">ESPRESSO</option>
+                            <option value="milk">MILK</option>
+                            <option value="syrup">SYRUP</option>
+                            </select>
+                            <input class="input_value" type="number" name="ingredient-value" value="" placeholder="VALUE" required>
+                            <button class="remove-ingredient" type="button" name="remove-ingredient"><i class="fas fa-minus"></i></button>
+                        </div>
+                    </li>
                     <li class="ingredient-desc">
                         <div class="ingredient-value">
                             <select class="ingredients-select" name="ingredients-select">
@@ -41,10 +59,11 @@ const create = `
               </ol>
               <button id="add-ingredient" type="button" name="add-ingredient"><i class="fas fa-plus"></i></button>
             </div>
-
-            <textarea id="description" name="name" rows="3" placeholder="DESCRIPTION"></textarea>
-            <button id="create-button" type="submit" name="create-button">CREATE</button>
           </div>
+        </div>
+        <div class="create-final">
+          <textarea id="description" name="name" rows="3" placeholder="DESCRIPTION"></textarea>
+          <button id="create-button" type="submit" name="create-button">CREATE</button>
         </div>
       </form>
 </section>
