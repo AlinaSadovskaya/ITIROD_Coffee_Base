b1039822ccf7bac8e4b0b58a9d1754eb25af8508
AlinaSadovskaya <alinasadovsckaya@yandex.ru>
2021-04-11 15:07:58 +0300

    add search

---
+++ db/search.js
@@ -0,0 +1,17 @@
+async function searchCoffee() {
+    let searchTextPrepair = document.getElementById("search-input");
+    let searchText = searchTextPrepair.value.trim().toLowerCase();
+    searchTextPrepair.value = "";
+    if (searchText != "") {
+        let catalog = await db.getAllDrinks();
+
+        for (let id in catalog) {
+            let coffeeName = catalog[id].coffeeName.toLowerCase();
+            if (coffeeName.includes(searchText) || searchText.includes(coffeeName)) {
+                onNavigate('/details?id=' + id);
+                return;
+            }
+        }
+        alert('A drink with this name was not found( \nYou can create it by going to the "create" tab!');
+    }
+}
