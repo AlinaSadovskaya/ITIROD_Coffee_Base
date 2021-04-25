81e8181b9271aa3d4fffdcf6985dda2db7313332
AlinaSadovskaya <alinasadovsckaya@yandex.ru>
2021-04-25 23:24:00 +0300

    add search list

--- db/search.js
+++ db/search.js
@@ -1,17 +1,86 @@
 async function searchCoffee() {
     let searchTextPrepair = document.getElementById("search-input");
     let searchText = searchTextPrepair.value.trim().toLowerCase();
-    searchTextPrepair.value = "";
+    //searchTextPrepair.value = "";
     if (searchText != "") {
         let catalog = await db.getAllDrinks();
-
+        let catalogArray = [];
+        for (let coffeeId in catalog) {
+            catalogArray.push({ id: coffeeId, value: catalog[coffeeId] });
+        }
+        catalog = catalogArray.reverse();
+        let newCatalog = [];
         for (let id in catalog) {
-            let coffeeName = catalog[id].coffeeName.toLowerCase();
+            let coffeeName = catalog[id].value.coffeeName.toLowerCase();
+
             if (coffeeName.includes(searchText) || searchText.includes(coffeeName)) {
-                onNavigate('/details?id=' + id);
-                return;
+                newCatalog.push(catalog[id]);
             }
         }
-        alert('A drink with this name was not found( \nYou can create it by going to the "create" tab! \nP.s. You must be registered to be able to create a drink');
+        if (newCatalog.length > 0) {
+            newCatalog.sort(function (a, b) {
+                var nameA = a.value.coffeeName.toLowerCase(), nameB = b.value.coffeeName.toLowerCase();
+                if (nameA.indexOf(searchText) < nameB.indexOf(searchText)) {
+                    return -1;
+                }
+                if (nameA.indexOf(searchText) > nameB.indexOf(searchText)) {
+                    return 1;
+                }
+                if (nameA.indexOf(searchText) == nameB.indexOf(searchText)) {
+                    if (nameA < nameB) {
+                        return -1;
+                    }
+                    if (nameA > nameB) {
+                        return 1;
+                    }
+                    else return 0;
+                }
+
+            });
+            //console.log(newCatalog);
+            newCatalog = newCatalog.slice(0, 3);
+            newCatalog = newCatalog.reverse();
+
+            let searchResult = document.getElementById('search-result');
+            // searchResult.style.display = 'block';
+            searchResult.innerHTML = '';
+            for (let coffee of newCatalog) {
+                let coffeeNode = getSearchItem(coffee);
+                searchResult.prepend(coffeeNode);
+            }
+        }
+        else alert('A drink with this name was not found( \nYou can create it by going to the "create" tab! \nP.s. You must be registered to be able to create a drink');
     }
+    else {
+        let searchResult = document.getElementById('search-result');
+        searchResult.innerHTML = '';
+        alert('Empty string to search!');
+    }
+}
+
+
+function getSearchItem(coffee) {
+    let result = document.createElement("li");
+    let coffeeNode = document.createElement("a");
+
+    let coffeeImg = document.createElement('i');
+    coffeeImg.className = 'fas fa-mug-hot';
+
+    let coffeeName = document.createElement("p");
+    coffeeName.textContent = coffee.value.coffeeName.toUpperCase();
+    //console.log(coffee);
+    coffeeNode.setAttribute('onclick',
+        `let searchTextPrepair = document.getElementById("search-input");
+         searchTextPrepair.value = "";
+         let searchResult = document.getElementById('search-result');
+         searchResult.innerHTML = '';
+         //searchResult.style.display = 'none';
+         onNavigate('/details?id=${coffee.id}'); 
+         return false;`);
+
+
+    coffeeNode.appendChild(coffeeImg);
+    coffeeNode.appendChild(coffeeName);
+    result.appendChild(coffeeNode);
+    return result;
 }
